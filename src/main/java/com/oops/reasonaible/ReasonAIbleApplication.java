de199bc35317263915538fd8af5e78115d71abbc
yyysolhhh <solbaram37@gmail.com>
2025-04-15 15:57:06 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/oops/reasonaible/ReasonAIbleApplication.java
@@ -0,0 +1,13 @@
+package com.oops.reasonaible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReasonAIbleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReasonAIbleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oops/reasonaible/ReasonAIbleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oops.reasonaible;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReasonAIbleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
