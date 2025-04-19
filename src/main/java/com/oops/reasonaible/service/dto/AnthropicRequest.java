65cf684a54f5730e12e555c237d78c485db30fb5
yyysolhhh <solbaram37@gmail.com>
2025-04-20 08:10:57 +0900

    fix: anthropic dto 수정
    AnthropicRequest
    - temperature 주석처리
    - create 메서드 인자에 model 추가
    
    Message
    - user 메서드 추가
    
    AnthropicResponse
    - content만 남김

--- src/main/java/com/oops/reasonaible/service/dto/AnthropicRequest.java
+++ src/main/java/com/oops/reasonaible/service/dto/AnthropicRequest.java
@@ -9,12 +9,21 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 public record AnthropicRequest(
 	String model,
 	List<Message> messages,
-	int maxTokens
+	// Double temperature,
+	Integer max_tokens
 ) {
+
+	public static AnthropicRequest create(String model, Message message) {
+		return new AnthropicRequest(model, List.of(Message.user(message.content())), 100);
+		// return new AnthropicRequest("claude-3-haiku-20240307", message, 0.7, 1000);
+	}
 
 	public record Message(
 		String role,
 		String content
 	) {
+		public static Message user(String content) {
+			return new Message("user", content);
+		}
 	}
 }
