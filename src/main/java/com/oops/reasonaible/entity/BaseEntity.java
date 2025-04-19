2352d79d14ab5660fa784cb1dffe14e15b965623
yyysolhhh <solbaram37@gmail.com>
2025-04-20 07:38:37 +0900

    feat: BaseEntity 생성, situation, excuse, modifiedExcuse 컬럼 TEXT로 설정
    BaseEntity
    - createdAt, updatedAt

---
+++ src/main/java/com/oops/reasonaible/entity/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.oops.reasonaible.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+	@CreatedDate
+	@Column(nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+	@LastModifiedDate
+	@Column(nullable = false)
+	private LocalDateTime updatedAt;
+}
