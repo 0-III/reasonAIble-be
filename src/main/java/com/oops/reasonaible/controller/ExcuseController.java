56872e8bb3200365c8bb75a5ccacdd3f915aaa78
yyysolhhh <solbaram37@gmail.com>
2025-04-20 23:53:39 +0900

    fix: RequestParam -> PathVariable, snakecase로 수정

--- src/main/java/com/oops/reasonaible/controller/ExcuseController.java
+++ src/main/java/com/oops/reasonaible/controller/ExcuseController.java
@@ -7,10 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.oops.reasonaible.service.ExcuseService;
@@ -45,26 +45,26 @@ public class ExcuseController {
 			ApiResponse.of(200, allExcuses));
 	}
 
-	@GetMapping("/{excuse_id}")
+	@GetMapping("/{excuseId}")
 	public ResponseEntity<ApiResponse<ExcuseGetResponse>> getExcuse(
-		@RequestParam Long excuseId) {
+		@PathVariable Long excuseId) {
 		ExcuseGetResponse excuse = excuseService.getExcuse(excuseId);
 		return ResponseEntity.status(HttpStatus.OK).body(
 			ApiResponse.of(200, excuse));
 	}
 
-	@PatchMapping("/{excuse_id}")
+	@PatchMapping("/{excuseId}")
 	public ResponseEntity<ApiResponse<ExcuseCreateUpdateResponse>> updateExcuse(
-		@RequestParam Long excuseId,
+		@PathVariable Long excuseId,
 		@RequestBody @Valid ExcuseUpdateRequest excuseUpdateRequest) {
 		ExcuseCreateUpdateResponse excuse = excuseService.updateExcuse(excuseId, excuseUpdateRequest);
 		return ResponseEntity.status(HttpStatus.OK).body(
 			ApiResponse.of(200, excuse));
 	}
 
-	@DeleteMapping("/{excuse_id}")
+	@DeleteMapping("/{excuseId}")
 	public ResponseEntity<ApiResponse> deleteExcuse(
-		@RequestParam Long excuseId) {
+		@PathVariable Long excuseId) {
 		excuseService.deleteExcuse(excuseId);
 		return ResponseEntity.status(HttpStatus.OK).body(
 			ApiResponse.of(200, "삭제 완료"));
