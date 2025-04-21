3879d13789fda5b458c33487b974bbc9fe0b75cb
yyysolhhh <solbaram37@gmail.com>
2025-04-21 13:03:10 +0900

    chore: client url2 추가

--- src/main/java/com/oops/reasonaible/config/WebConfig.java
+++ src/main/java/com/oops/reasonaible/config/WebConfig.java
@@ -9,11 +9,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 	@Value("${client.url}")
 	private String clientUrl;
+	@Value("${client.url2}")
+	private String clientUrl2;
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/api/**")
 			.allowedOrigins(clientUrl)
+			.allowedOrigins(clientUrl2)
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
 			.allowedHeaders("*")
 			.allowCredentials(true);
