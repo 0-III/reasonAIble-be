ce3e95166ae6f4e650e2e1f0e3b70bec98fa559c
yyysolhhh <solbaram37@gmail.com>
2025-04-21 10:56:56 +0900

    fix: cors 설정에 clientUrl 추가

--- src/main/java/com/oops/reasonaible/config/WebConfig.java
+++ src/main/java/com/oops/reasonaible/config/WebConfig.java
@@ -1,16 +1,20 @@
 package com.oops.reasonaible.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-	
+	@Value("${client.url}")
+	private String clientUrl;
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/api/**")
 			.allowedOrigins("http://localhost:3000")
+			.allowedOrigins(clientUrl)
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
 			.allowedHeaders("*")
 			.allowCredentials(true);
