93ee0d914eb6e8249dc00ce576fd986c192766ff
yyysolhhh <solbaram37@gmail.com>
2025-04-25 17:46:07 +0900

    feat: security 이용한 로그인 기능 구현
    SecurityConfig
    - filterChain
    - jwtAuthenticationFilter
    - customAuthenticationProcessingFilter
    - loginFailureHandler
    - loginSuccessHandler
    - authenticationManager
    - passwordEncoder
    - 스프링 빈으로 등록
    
    CustomAuthenticationProcessingFilter
    - AbstractAuthenticationProcessingFilter 상속받은 클래스
    - attemptAuthentication 오버라이딩
    - DEFAULT_LOGIN_REQUEST_URL 설정
    
    CustomUserDetailsService
    - UserDetailsService 인터페이스 구현
    - loadUserByUsername 오버라이딩
    
    MemberRepository
    - findByEmail 메서드 추가

--- src/main/java/com/oops/reasonaible/member/repository/MemberRepository.java
+++ src/main/java/com/oops/reasonaible/member/repository/MemberRepository.java
@@ -1,8 +1,12 @@
 package com.oops.reasonaible.member.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.oops.reasonaible.member.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+	Optional<Member> findByEmail(String email);
 }
