9ef9d84ac9a8a1f5d943ac08cff30d480a07ba4f
yyysolhhh <solbaram37@gmail.com>
2025-04-25 17:28:21 +0900

    feat: BAD_REQUEST, EMAIL_ALREADY_EXISTS, INVALID_ACCESS_TOKEN 추가
    - NOT_FOUND 메시지 한국어로 수정

--- src/main/java/com/oops/reasonaible/core/exception/ErrorCode.java
+++ src/main/java/com/oops/reasonaible/core/exception/ErrorCode.java
@@ -8,8 +8,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-	NOT_FOUND(HttpStatus.NOT_FOUND, "Not Found"),
-	;
+	NOT_FOUND(HttpStatus.NOT_FOUND, "찾을 수 없는 리소스입니다."),
+	BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+	EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "중복된 이메일입니다."),
+	INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 엑세스 토큰입니다.");;
 
 	private final HttpStatus httpStatus;
 	private final String message;
