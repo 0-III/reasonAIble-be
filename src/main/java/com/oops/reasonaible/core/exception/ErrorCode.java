eb8b997b47f92e7ba8e4db4333dd7cf2213a6dc2
yyysolhhh <solbaram37@gmail.com>
2025-04-27 21:29:17 +0900

    feat: INVALID_ACCESS_TOKEN, FORBIDDEN, USER_NOT_FOUND 추가

--- src/main/java/com/oops/reasonaible/core/exception/ErrorCode.java
+++ src/main/java/com/oops/reasonaible/core/exception/ErrorCode.java
@@ -11,7 +11,10 @@ public enum ErrorCode {
 	NOT_FOUND(HttpStatus.NOT_FOUND, "찾을 수 없는 리소스입니다."),
 	BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
 	EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "중복된 이메일입니다."),
-	INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 엑세스 토큰입니다.");;
+	INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 엑세스 토큰입니다."),
+	FORBIDDEN(HttpStatus.FORBIDDEN, "접근이 금지된 리소스입니다."),
+	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+	;
 
 	private final HttpStatus httpStatus;
 	private final String message;
