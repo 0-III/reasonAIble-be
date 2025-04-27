d15c73db626622bbe2ded70772a109658ebc2cb5
yyysolhhh <solbaram37@gmail.com>
2025-04-27 18:36:57 +0900

    feat: filterChain에 httpBasic, session 설정 추가

--- src/main/java/com/oops/reasonaible/core/config/SecurityConfig.java
+++ src/main/java/com/oops/reasonaible/core/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -29,6 +30,8 @@ import jakarta.servlet.Filter;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
+// @EnableReactiveMethodSecurity
+// @EnableWebFluxSecurity
 @EnableWebSecurity
 @RequiredArgsConstructor
 @Configuration
@@ -44,10 +47,12 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf(AbstractHttpConfigurer::disable)
 			.formLogin(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests(auth -> auth
 					.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-					.requestMatchers("/api/v1/members", "/api/v1/members/**").permitAll()
-					.anyRequest().authenticated() // TODO: 나중에 인증 필요로 변경
+					.requestMatchers("/api/v1/members").permitAll()
+					.anyRequest().authenticated()
 				// .anyRequest().permitAll()
 			)
 			.addFilterAfter(customAuthenticationProcessingFilter(), LogoutFilter.class)
